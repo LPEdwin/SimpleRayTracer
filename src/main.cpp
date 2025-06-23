da6c6ccf8a2a7c84135e11d49e64702ef43fdd48
NB <n.baumgaertner@hotmail.com>
2025-06-24 01:22:27 +0200

    added gradient

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 #include <fstream>
 #include <cstdint>
 #include <vector>
@@ -13,43 +13,45 @@
 using namespace std;
 using namespace std::numbers;
 
-void render(const Camera& camera, const Sphere& sphere, Image& image) {
-    
-    for (int y = 0; y < image.height; ++y) {
-        for (int x = 0; x < image.width; ++x) {            
-            float aspectRatio = static_cast<float>(image.width) / image.height;
-            float pixelNDCX = (x + 0.5f) / image.width;
-            float pixelNDCY = (y + 0.5f) / image.height;
-            float pixelScreenX = (2.0f * pixelNDCX - 1.0f) * aspectRatio * tan(camera.fovInDegree * 0.5f * pi / 180.0f);
-            float pixelScreenY = (1.0f - 2.0f * pixelNDCY) * tan(camera.fovInDegree * 0.5f * pi / 180.0f);
-
-            vec3 rayDirection(pixelScreenX, pixelScreenY, -1);
-            rayDirection = unit_vector(rayDirection); // Normalize the ray direction
+void render(const Camera &camera, const Sphere &sphere, Image &image)
+{
+    const vec3 pixelDelta = vec3(1.0f / image.width, 1.0f / image.height, 0.0f);
+    for (int y = 0; y < image.height; ++y)
+    {
+        for (int x = 0; x < image.width; ++x)
+        {
+            Ray ray = camera.get_ray(x * pixelDelta.x(), pixelDelta.y() * (image.height - 1 - y));
+            auto dir = unit_vector(ray.direction);
             float t = 0.0f;
-            if(sphere.intersect(Ray(camera.position, rayDirection), t)) {
-                image.pixels[y][x] = vec3(1.0f, 0.0f, 0.0f); 
-            } else {
-                image.pixels[y][x] = vec3(0, 0, 0);
+            if (sphere.intersect(Ray(camera.position, dir), t))
+            {
+                image.pixels[y][x] = vec3(1.0f, 0.0f, 0.0f);
+            }
+            else
+            {
+                auto a = 0.5 * (dir.y() + 1.0);
+                image.pixels[y][x] = (1.0 - a) * vec3(1.0, 1.0, 1.0) + a * vec3(0.5, 0.7, 1.0);
             }
         }
-    }    
+    }
 };
 
-
-
-
-int main(){
-    Camera camera(vec3(0, 0, 5), vec3(0, 0, -1), 60.0f, 16.0/9.0);
+int main()
+{
+    Camera camera(vec3(0, 0, 5), vec3(0, 0, -1), 60.0f, 16.0 / 9.0);
     Sphere sphere(vec3(0, 0, 0), 1.0f);
-    Image image(800, 600);
+    Image image(1280, 720);
 
     render(camera, sphere, image);
 
-    try {
+    try
+    {
         auto filename = "output.bmp";
         writeImageToBMP(image, filename);
         std::cout << "BMP saved to " << filename << "\n";
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception &e)
+    {
         std::cerr << "Error writing BMP: " << e.what() << "\n";
     }
 
