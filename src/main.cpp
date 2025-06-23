08fef68b19ee70ae03d656f494d5a43bdf95bd0a
NB <n.baumgaertner@hotmail.com>
2025-06-23 15:41:27 +0200

    imported shirleys vec3 implementation

--- src/main.cpp
+++ src/main.cpp
@@ -3,54 +3,36 @@
 #include <fstream>
 #include <cstdint>
 #include <vector>
+#include "vec3.cpp"
 using namespace std;
 
 const float PI = 3.14159265358979323846;
 
-struct Vector3 {
-    float x, y, z;
-    Vector3() : x(0), y(0), z(0) {}
-    Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
-
-    Vector3 operator-(const Vector3& other) const {
-        return Vector3(x - other.x, y - other.y, z - other.z);
-    }
-
-    float length() const {
-        return sqrt(x*x + y*y + z*z);
-    }
-
-    Vector3 normalized() const {
-        float len = length();
-        return (len > 0) ? Vector3(x/len, y/len, z/len) : Vector3(0,0,0);
-    }
-};
-
 struct Ray {
-    Vector3 origin;
-    Vector3 direction;    
-    Ray(Vector3 o, Vector3 d) : origin(o), direction(d) {}
+    vec3 origin;
+    vec3 direction;
+    Ray(vec3 o, vec3 d) : origin(o), direction(d) {}
 };
 
 struct Camera {
-    Vector3 position;
-    Vector3 direction;
+    vec3 position;
+    vec3 direction;
     float fov;
     Camera() : position(), direction(0, 0, -1), fov(60.0f) {}
-    Camera(Vector3 pos, Vector3 dir, float fov) : position(pos), direction(dir), fov(fov) {}
+    Camera(vec3 pos, vec3 dir, float fov) : position(pos), direction(dir), fov(fov) {}
 };
 
 struct Sphere {
-    Vector3 center;
+    vec3 center;
     float radius;
     Sphere() : center(), radius(1.0f) {}
-    Sphere(Vector3 c, float r) : center(c), radius(r) {}
+    Sphere(vec3 c, float r) : center(c), radius(r) {}
 
     bool intersect(const Ray& ray, float& t) const {
-        Vector3 oc = ray.origin - center;
-        float a = ray.direction.x * ray.direction.x + ray.direction.y * ray.direction.y + ray.direction.z * ray.direction.z;
-        float b = 2.0f * (oc.x * ray.direction.x + oc.y * ray.direction.y + oc.z * ray.direction.z);
-        float c = oc.x * oc.x + oc.y * oc.y + oc.z * oc.z - radius * radius;
+        vec3 oc = ray.origin - center;
+        float a = dot(ray.direction, ray.direction);
+        float b = 2.0f * dot(oc, ray.direction);
+        float c = dot(oc, oc) - radius * radius;
         float discriminant = b * b - 4 * a * c;
         if (discriminant < 0) {
             return false; 
@@ -63,12 +45,12 @@ struct Sphere {
 
 struct Image{
     int width, height;
-    Vector3** pixels;
+    vec3** pixels;
 
     Image(int w, int h) : width(w), height(h) {
-        pixels = new Vector3*[height];
+        pixels = new vec3*[height];
         for (int i = 0; i < height; ++i) {
-            pixels[i] = new Vector3[width];
+            pixels[i] = new vec3[width];
         }
     }
 
@@ -91,13 +73,13 @@ void render(const Camera& camera, const Sphere& sphere, Image& image) {
             float pixelScreenX = (2.0f * pixelNDCX - 1.0f) * aspectRatio * tan(camera.fov * 0.5f * PI / 180.0f);
             float pixelScreenY = (1.0f - 2.0f * pixelNDCY) * tan(camera.fov * 0.5f * PI / 180.0f);
 
-            Vector3 rayDirection(pixelScreenX, pixelScreenY, -1);
-            rayDirection = rayDirection.normalized();
+            vec3 rayDirection(pixelScreenX, pixelScreenY, -1);
+            rayDirection = unit_vector(rayDirection); // Normalize the ray direction
             float t = 0.0f;
             if(sphere.intersect(Ray(camera.position, rayDirection), t)) {
-                image.pixels[y][x] = Vector3(1.0f, 0.0f, 0.0f); 
+                image.pixels[y][x] = vec3(1.0f, 0.0f, 0.0f); 
             } else {
-                image.pixels[y][x] = Vector3(0, 0, 0);
+                image.pixels[y][x] = vec3(0, 0, 0);
             }
         }
     }    
@@ -176,9 +158,9 @@ void writeImageToBMP(const Image& image, const std::string& filename) {
                 return static_cast<uint8_t>(c * 255.0f);
             };
 
-            uint8_t r = clamp(image.pixels[y][x].x);
-            uint8_t g = clamp(image.pixels[y][x].y);
-            uint8_t b = clamp(image.pixels[y][x].z);
+            uint8_t r = clamp(image.pixels[y][x].x());
+            uint8_t g = clamp(image.pixels[y][x].y());
+            uint8_t b = clamp(image.pixels[y][x].z());
 
             // BMP uses BGR order
             ofs.put(b);
@@ -194,8 +176,8 @@ void writeImageToBMP(const Image& image, const std::string& filename) {
 
 
 int main(){
-    Camera camera(Vector3(0, 0, 5), Vector3(0, 0, -1), 60.0f);
-    Sphere sphere(Vector3(0, 0, 0), 1.0f);
+    Camera camera(vec3(0, 0, 5), vec3(0, 0, -1), 60.0f);
+    Sphere sphere(vec3(0, 0, 0), 1.0f);
     Image image(800, 600);
 
     render(camera, sphere, image);
