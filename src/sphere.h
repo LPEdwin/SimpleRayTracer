9d12b373a9a51fea38fb1ae81d580c6fa0b36ec3
NB <n.baumgaertner@hotmail.com>
2025-06-24 02:22:10 +0200

    faster implementation

--- src/sphere.h
+++ src/sphere.h
@@ -7,24 +7,24 @@ struct Sphere
 {
     Vector3 center;
     float radius;
-    
+
     Sphere() : center(), radius(1.0f) {}
     Sphere(Vector3 c, float r) : center(c), radius(r) {}
 
     bool Intersect(const Ray &ray, float &t) const
     {
-        Vector3 oc = ray.origin - center;
-        float a = Dot(ray.direction, ray.direction);
-        float b = 2.0f * Dot(oc, ray.direction);
-        float c = Dot(oc, oc) - radius * radius;
-        float discriminant = b * b - 4 * a * c;
+        Vector3 oc = center - ray.origin;
+        float a = ray.direction.LengthSquared();
+        float b = Dot(oc, ray.direction);
+        float c = oc.LengthSquared() - radius * radius;
+        float discriminant = b * b - a * c;
         if (discriminant < 0)
         {
             return false;
         }
         else
         {
-            t = (-b - sqrt(discriminant)) / (2.0f * a);
+            t = (-b - sqrt(discriminant)) / a;
             return true;
         }
     }
