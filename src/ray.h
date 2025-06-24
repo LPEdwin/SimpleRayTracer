1c4e45f2ce55a96cde44f6e8bf617e98c567ba79
NB <n.baumgaertner@hotmail.com>
2025-06-24 19:35:32 +0200

    made uppercase

--- src/ray.h
+++ src/ray.h
@@ -5,13 +5,13 @@
 class Ray
 {
 public:
-    Ray(const point3 &origin, const Vector3 &direction, double time = 0.0) : origin(origin), direction(direction), time(time) {}
+    Ray(const Point3 &origin, const Vector3 &direction, double time = 0.0) : origin(origin), direction(direction), time(time) {}
 
-    const point3 origin;
+    const Point3 origin;
     const Vector3 direction;
     double const time;
 
-    point3 At(double t) const
+    Point3 At(double t) const
     {
         return origin + t * direction;
     }
--- src/vector3.h
+++ src/vector3.h
@@ -79,8 +79,9 @@ public:
     }
 };
 
-// point3 is just an alias for vec3, but useful for geometric clarity in the code.
-using point3 = Vector3;
+// alias for common types
+using Point3 = Vector3;
+using Color = Vector3;
 
 // Vector Utility Functions
 
