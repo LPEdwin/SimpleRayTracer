6af30576d0e4790bdf00085dd93e16adc01936d6
NB <n.baumgaertner@hotmail.com>
2025-06-25 02:55:39 +0200

    added anti aliasing

--- src/render.h
+++ src/render.h
@@ -11,7 +11,7 @@
 
 double inf = std::numeric_limits<double>::infinity();
 
-void Render(const Camera &camera, const Hittable &world, Image &image)
+void Render(const Camera &camera, const Hittable &world, Image &image, int samplesPerPixel = 10)
 {
     const Vector3 pixelDelta = Vector3(1.0f / image.width, 1.0f / image.height, 0.0f);
 
@@ -19,18 +19,25 @@ void Render(const Camera &camera, const Hittable &world, Image &image)
     {
         for (int x = 0; x < image.width; ++x)
         {
-            Ray ray = camera.GetRay(x * pixelDelta.x(), pixelDelta.y() * (image.height - 1 - y));
-            auto dir = UnitVector(ray.direction);
+            Color color(0, 0, 0);
+            for (int s = 0; s < samplesPerPixel; ++s)
+            {
+                auto aaOffset = Vector3(RandomDouble() - 0.5, RandomDouble() - 0.5, 0.0);
+                Ray ray = camera.GetRay((x + aaOffset.x()) * pixelDelta.x(),
+                                        (y + aaOffset.y()) * pixelDelta.y());
+                auto dir = UnitVector(ray.direction);
 
-            if (auto hit = world.Hit(ray, 0.0, inf))
-            {
-                image.pixels[y][x] = 0.5 * (hit->normal + Color(1, 1, 1));
+                if (auto hit = world.Hit(ray, 0.0, inf))
+                {
+                    color += 0.5 * (hit->normal + Color(1, 1, 1));
+                }
+                else
+                {
+                    auto a = 0.5 * (dir.y() + 1.0);
+                    color += (1.0 - a) * Vector3(1.0, 1.0, 1.0) + a * Vector3(0.5, 0.7, 1.0);
+                }
             }
-            else
-            {
-                auto a = 0.5 * (dir.y() + 1.0);
-                image.pixels[y][x] = (1.0 - a) * Vector3(1.0, 1.0, 1.0) + a * Vector3(0.5, 0.7, 1.0);
-            }
+            image.pixels[image.height - 1 - y][x] = color / samplesPerPixel;
         }
     }
 };
