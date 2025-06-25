c4cb2469d75a010999210ef5e1712fb8112316b9
NB <n.baumgaertner@hotmail.com>
2025-06-25 03:29:35 +0200

    implemented secondary rays

--- src/render.h
+++ src/render.h
@@ -9,7 +9,23 @@
 #include "vector3.h"
 #include "hittable.h"
 
-double inf = std::numeric_limits<double>::infinity();
+static const double inf = std::numeric_limits<double>::infinity();
+
+Color GetColor(const Ray &ray, const Hittable &world, int depth = 50)
+{
+    if (depth <= 0)
+        return Color(0, 0, 0);
+
+    if (auto hit = world.Hit(ray, 0.001, inf))
+    {
+        auto r2 = RandomOnHemisphere(hit->normal);
+        return 0.5 * GetColor(Ray(hit->point, r2, ray.time), world, depth - 1);
+    }
+
+    auto dir = UnitVector(ray.direction);
+    auto a = 0.5 * (dir.y() + 1.0);
+    return (1.0 - a) * Vector3(1.0, 1.0, 1.0) + a * Vector3(0.5, 0.7, 1.0);
+}
 
 void Render(const Camera &camera, const Hittable &world, Image &image, int samplesPerPixel = 10)
 {
@@ -25,19 +41,9 @@ void Render(const Camera &camera, const Hittable &world, Image &image, int sampl
                 auto aaOffset = Vector3(RandomDouble() - 0.5, RandomDouble() - 0.5, 0.0);
                 Ray ray = camera.GetRay((x + aaOffset.x()) * pixelDelta.x(),
                                         (y + aaOffset.y()) * pixelDelta.y());
-                auto dir = UnitVector(ray.direction);
-
-                if (auto hit = world.Hit(ray, 0.0, inf))
-                {
-                    color += 0.5 * (hit->normal + Color(1, 1, 1));
-                }
-                else
-                {
-                    auto a = 0.5 * (dir.y() + 1.0);
-                    color += (1.0 - a) * Vector3(1.0, 1.0, 1.0) + a * Vector3(0.5, 0.7, 1.0);
-                }
+                color += GetColor(ray, world);
             }
             image.pixels[image.height - 1 - y][x] = color / samplesPerPixel;
         }
     }
-};
+}
