151799ee4850e5997f007b3cc25cbfa56d31cb0e
NB <n.baumgaertner@hotmail.com>
2025-06-27 01:46:27 +0200

    refactor

--- src/render.h
+++ src/render.h
@@ -8,6 +8,7 @@
 #include "ray.h"
 #include "vector3.h"
 #include "hittable.h"
+#include "progress.h"
 
 static const double inf = std::numeric_limits<double>::infinity();
 
@@ -33,24 +34,6 @@ Color GetColor(const Ray &ray, const Hittable &world, int depth = 50)
     return (1.0 - a) * Vector3(1.0, 1.0, 1.0) + a * Vector3(0.5, 0.7, 1.0);
 }
 
-void updateProgress(int current, int total)
-{
-    static bool first_call = true;
-    if (first_call)
-    {
-        std::cout << "\033[?25l"; // Hide cursor
-        first_call = false;
-    }
-
-    int percent = static_cast<int>(100.0 * current / total);
-    std::cout << "\rProgress: " << std::setw(3) << percent << "% completed" << std::flush;
-
-    if (current == total)
-    {
-        std::cout << "\033[?25h" << std::endl; // Show cursor and newline
-    }
-}
-
 void Render(const Camera &camera, const Hittable &world, Image &image, int samplesPerPixel = 100)
 {
     const Vector3 pixelDelta = Vector3(1.0f / image.width, 1.0f / image.height, 0.0f);
@@ -69,6 +52,6 @@ void Render(const Camera &camera, const Hittable &world, Image &image, int sampl
             }
             image.pixels[y][x] = color / samplesPerPixel;
         }
-        updateProgress(y + 1, image.height);
+        ShowProgress(y + 1, image.height);
     }
 }
