e6eefd8c8d493023afe03174f2fb8785c584b7d4
NB <n.baumgaertner@hotmail.com>
2025-07-04 01:10:42 +0200

    added

---
+++ src/instance.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "hittable.h"
+#include "transform.h"
+#include "aabb.h"
+#include "ray.h"
+
+class Instance : public Hittable
+{
+private:
+    shared_ptr<Hittable> hittable;
+    Transform transform;
+    Transform inverse_transform;
+    AABB bbox;
+
+public:
+    Instance(shared_ptr<Hittable> hittable, const Transform &transform)
+        : hittable(hittable),
+          transform(transform),
+          inverse_transform(transform.Inverse()),
+          bbox(AABB::Transformed(hittable->BoundingBox(), transform)) {}
+
+    AABB BoundingBox() const override
+    {
+        return bbox;
+    }
+
+    bool Hit(const Ray &ray, HitResult &hit, double t_min, double t_max) const override
+    {
+        // Transform the ray to the object's local space
+        Ray local_ray(inverse_transform * ray.origin,
+                      inverse_transform.TransformVector(ray.direction),
+                      ray.time);
+
+        // Call the underlying hittable's Hit method
+        if (hittable->Hit(local_ray, hit, t_min, t_max))
+        {
+            // Transform the hit record back to world space
+            hit.point = transform * hit.point;
+            hit.normal = transform.TransformNormal(hit.normal);
+            return true;
+        }
+        return false;
+    }
+};
