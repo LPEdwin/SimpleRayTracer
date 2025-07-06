7901485a02722ade5e01da62a5e2e298461269fd
NB <n.baumgaertner@hotmail.com>
2025-07-06 02:23:56 +0200

    added bvh

--- src/scenes/mesh_test.h
+++ src/scenes/mesh_test.h
@@ -5,13 +5,17 @@
 #include "core/camera.h"
 #include "collision/hittable_list.h"
 #include "collision/instance.h"
+#include "collision/bvh_node.h"
 
-Scene MeshTest(string file)
+Scene MeshTest(string file, bool useBvh = false)
 {
     auto triangles = LoadObjectFile(file);
     auto cam = std::make_shared<Camera>(Vector3(0, 278, -800), Vector3(0, 0, 0), 40.0, 1.0);
     auto hittables = AsHittables(triangles);
-    auto mesh = std::make_shared<HittableList>(hittables);
+    shared_ptr<Hittable> mesh = std::make_shared<HittableList>(hittables);
+    if (useBvh)
+        mesh = BvhNode::Build(hittables);
+    mesh = BvhNode::Build(hittables);
     auto scale = 250.0 / mesh->BoundingBox().LongestAxis().Length();
     auto scaled = std::make_shared<Instance>(mesh, Transform::FromScale(scale).RotateY(180));
     fmt::println("BB Scaled Mesh: {}", scaled->BoundingBox());
