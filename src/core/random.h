77ccc2713059db11a9decc5df4d3ccd44ca87c49
LPEdwin <n.baumgaertner@gmail.com>
2026-03-06 04:19:19 +0100

    Add SetGlobalRandomSeed function

--- src/core/random.h
+++ src/core/random.h
@@ -2,11 +2,21 @@
 
 #include <random>
 
-double RandomDouble()
+inline std::mt19937 &GlobalRng()
 {
     static thread_local std::mt19937 rng(std::random_device{}());
+    return rng;
+}
+
+inline void SetGlobalRandomSeed(uint32_t seed)
+{
+    GlobalRng().seed(seed);
+}
+
+inline double RandomDouble()
+{
     static thread_local std::uniform_real_distribution<double> dist(0.0, 1.0);
-    return dist(rng);
+    return dist(GlobalRng());
 }
 
 inline double RandomDouble(double min, double max)
