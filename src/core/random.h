8cac1de5c7668a4b34fceb484dfa7883d66db164
LPEdwin <n.baumgaertner@gmail.com>
2026-03-09 03:22:11 +0100

    Add deterministic random

--- src/core/random.h
+++ src/core/random.h
@@ -23,3 +23,20 @@ inline double RandomDouble(double min, double max)
 {
     return min + (max - min) * RandomDouble();
 }
+
+inline std::mt19937 &FixedRng()
+{
+    static thread_local std::mt19937 rng(1337);
+    return rng;
+}
+
+inline double FixedRandomDouble()
+{
+    static thread_local std::uniform_real_distribution<double> dist(0.0, 1.0);
+    return dist(FixedRng());
+}
+
+inline double FixedRandomDouble(double min, double max)
+{
+    return min + (max - min) * FixedRandomDouble();
+}
